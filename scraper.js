c0343eb15e2fcaedbc3ae96d07d999b015c6b480
sicherteam <atsicherteam@gmail.com>
2026-07-29 03:27:31 +0200

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -10,7 +10,7 @@ puppeteer.use(StealthPlugin());
 // --- CONFIGURATION ---
 const CONFIG = {
   projectName: 'Osman Reklam',
-  userDataPath: '/home/ubuntu/osman-reklam/user_data',
+  userDataPath: '/home/ubuntu/test_chrome_profile',
   targetUrl: 'https://ads.google.com/localservices/inbox?cid=2903573653&bid=10985702078&pid=9999999999&euid=3547106212&hl=de-AT&gl=AT',
   telegramToken: process.env.TELEGRAM_BOT_TOKEN,
   telegramChatId: process.env.TELEGRAM_CHAT_ID,
@@ -88,7 +88,7 @@ function clearChromeLocks() {
 browser = await puppeteer.launch({
   headless: false,
   executablePath: '/usr/bin/google-chrome',
-  userDataDir: CONFIG.userDataPath,
+  userDataDir: '/home/ubuntu/test_chrome_profile',
   args: [
     '--no-sandbox',
     '--disable-setuid-sandbox',
