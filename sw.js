1f0491b184183b255baab98249565a2f427c1245
sicherteam <atsicherteam@gmail.com>
2026-07-31 14:29:37 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,3 @@
-// Minimal Service Worker (PWA Yükleme Şartını Sağlamak İçin)
 self.addEventListener('fetch', (event) => {
-  // İsteği olduğu gibi ağa yönlendirir, hiçbir şeyi önbelleğe almaz
+  // Minimal Service Worker (PWA Yükleme Şartını Sağlamak İçin)
 });
